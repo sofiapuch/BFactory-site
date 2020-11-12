c0d495135c4da666f2301485cce1b522a1f2dd30
Sofia Puch <sofiapuch.frontend@gmail.com>
2020-11-12 21:07:18 +0100

    Update logo

--- src/components/LargeHero/CompanyLogo/index.js
+++ src/components/LargeHero/CompanyLogo/index.js
@@ -5,9 +5,10 @@ import logo from '../../../assets/bfactory_logo.svg';
 import './CompanyLogo.scss';
 
 const companyLogo = () => {
+
     return (
         <div className="company-logo">
-            <div className="icon"><img src={ logo } alt="icon" /></div>
+            <div className="company-logo__logo" style={{backgroundImage: "url('" + logo + "')"}}></div>
         </div>
     )
 }
