ed3e034c03d624d1beef44d52a8e2044dd0a3cce
Sofia Puch <sofiapuch.frontend@gmail.com>
2020-11-12 14:22:47 +0100

    Styling updates for mobile

--- src/components/LargeHero/Address/index.js
+++ src/components/LargeHero/Address/index.js
@@ -13,25 +13,25 @@ const address = (props) => {
     return (
         <div className="address">
 
-            <div className="grid-x grid-margin-x">
-                <div className="cell medium-6">
-                    <div className="address__text-wrapper">
-                        <div className="icon"><img src={ pinSvg } alt="icon" /></div>
-                        <p className="address__text">{ street }</p>
-                    </div>
-                    <p className="address__text">{ postcode }</p>
+            <div className="address__block">
+                <div className="address__text-wrapper">
+                    <div className="icon"><img src={ pinSvg } alt="icon" /></div>
+                        <p className="address__text">{ street }<span className="address__text--mobile"> - { postcode }</span></p>
                 </div>
-                <div className="cell medium-6">
-                    <div className="address__text-wrapper">
-                        <div className="icon"><img src={ trainSvg } alt="icon" /></div>
-                        <p className="address__text">{ transport[0] } - { transport[1] }</p>
-                    </div>
-                    <div className="address__text-wrapper">
-                        <div className="icon"><img src={ phoneSvg } alt="icon" /></div>
-                        <p className="address__text">{ phone }</p>
-                    </div>
+                <p className="address__text address__text--desktop">{ postcode }</p>
+            </div>
+
+            <div className="address__block">
+                <div className="address__text-wrapper">
+                    <div className="icon"><img src={ trainSvg } alt="icon" /></div>
+                    <p className="address__text">{ transport[0] } - { transport[1] }</p>
+                </div>
+                <div className="address__text-wrapper">
+                    <div className="icon"><img src={ phoneSvg } alt="icon" /></div>
+                    <p className="address__text">{ phone }</p>
                 </div>
             </div>
+
         </div>
     )
 }
